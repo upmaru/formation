17f8ba6dce5c9275b0edc4a48811cb87ad6c92a8
Zack Siri <zacksiri@gmail.com>
2021-12-08 19:56:20 +0700

    update test config

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,6 @@
 Mox.defmock(Formation.LexdeeMock, for: Formation.Clients.Lexdee.Behaviour)
 Application.put_env(:formation, :lexdee, Formation.LexdeeMock)
 
+Application.put_env(:lexdee, :environment, :test)
+
 ExUnit.start()
