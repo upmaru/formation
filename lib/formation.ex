588015d32d1dbf4da83b9f5ba1cdba69460a2b8c
Zack Siri <me@zacksiri.dev>
2022-12-23 15:55:47 +0700

    Add test for alpine

--- lib/formation.ex
+++ lib/formation.ex
@@ -4,6 +4,22 @@ defmodule Formation do
   """
   alias Formation.Lxd
 
+  defdelegate lxd_start(client, slug),
+    to: Lxd,
+    as: :start
+
+  defdelegate lxd_create(client, slug, params),
+    to: Lxd,
+    as: :create
+
+  defdelegate lxd_stop(client, slug),
+    to: Lxd,
+    as: :stop
+
+  defdelegate lxd_delete(client, slug),
+    to: Lxd,
+    as: :delete
+
   defdelegate new_lxd_instance(params),
     to: Lxd.Instance,
     as: :new
@@ -16,11 +32,7 @@ defmodule Formation do
     to: Lxd.Instance,
     as: :add_package_and_restart
 
-  defdelegate lxd_create(client, slug, params),
-    to: Lxd,
-    as: :create
-
-  defdelegate lxd_start(client, slug),
-    to: Lxd,
-    as: :start
+  defdelegate lxd_upgrade_alpine_package(client, instance),
+    to: Lxd.Alpine,
+    as: :upgrade_package
 end
---
+++ test/formation/lxd/alpine_test.exs
@@ -0,0 +1,71 @@
+defmodule Formation.Lxd.AlpineTest do
+  use ExUnit.Case, async: true
+
+  import Mox
+
+  setup :verify_on_exit!
+
+  alias Formation.Lxd.Alpine
+
+  @uuid "some-operation-id"
+  @repositories []
+
+  setup do
+    client = Lexdee.create_client("http://localhost:1234")
+
+    instance =
+      Formation.new_lxd_instance(%{
+        slug: "some-instance-1",
+        repositories: @repositories,
+        package: %{slug: "some-package-slug"}
+      })
+
+    {:ok, client: client, instance: instance}
+  end
+
+  describe "upgrade package" do
+    setup do
+      Formation.LexdeeMock
+      |> expect(:execute_command, fn _client, "some-instance-1", command ->
+        assert "apk update && apk add --upgrade some-package-slug\n" == command
+
+        {:ok, %{body: %{"id" => @uuid}}}
+      end)
+
+      Formation.LexdeeMock
+      |> expect(:wait_for_operation, fn _client, _uuid, _options ->
+        {:ok,
+         %{
+           body: %{
+             "metadata" => %{
+               "output" => %{
+                 "1" => "stdout.log",
+                 "2" => "stderr.log"
+               }
+             }
+           }
+         }}
+      end)
+
+      Formation.LexdeeMock
+      |> expect(:show_instance_log, fn _client, "some-instance-1", "stdout.log" ->
+        {:ok, %{body: "some-url"}}
+      end)
+
+      Formation.LexdeeMock
+      |> expect(:show_instance_log, fn _client, "some-instance-1", "stderr.log" ->
+        {:ok, %{body: ""}}
+      end)
+
+      :ok
+    end
+
+    test "execute upgrade package", %{client: client, instance: instance} do
+      assert {:ok, _result} = Alpine.upgrade_package(client, instance)
+    end
+
+    test "execute upgrade package using formation module", %{client: client, instance: instance} do
+      assert {:ok, _result} = Formation.lxd_upgrade_alpine_package(client, instance)
+    end
+  end
+end
