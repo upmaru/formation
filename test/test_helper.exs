ee6d28f6e674e8a40bcd10f49c7315a104f95d15
Zack Siri <me@zacksiri.dev>
2023-11-22 11:18:30 +0700

    Remove vcr config from test_helper
    Signed-off-by: Zack Siri <me@zacksiri.dev>

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,5 +2,5 @@ Mox.defmock(Formation.LexdeeMock, for: Lexdee.Behaviour)
 Application.put_env(:formation, :lexdee, Formation.LexdeeMock)
 
 Application.put_env(:lexdee, :environment, :test)
-ExVCR.Config.cassette_library_dir("test/fixture/vcr_cassettes")
+
 ExUnit.start()
