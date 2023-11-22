4e2da0209710aa68f57baaf75b109a36d5e54702
Zack Siri <me@zacksiri.dev>
2023-11-22 13:39:29 +0700

    add parsing of generic credential
    Signed-off-by: Zack Siri <me@zacksiri.dev>

--- lib/formation/s3/credential.ex
+++ lib/formation/s3/credential.ex
@@ -14,6 +14,11 @@ defmodule Formation.S3.Credential do
 
   @primary_key false
   embedded_schema do
+    field :host, :string, virtual: true
+    field :username, :string, virtual: true
+    field :password, :string, virtual: true
+    field :resource, :string, virtual: true
+
     field :type, Ecto.Enum, values: [:component, :instance]
 
     field :endpoint, :string, default: "s3.amazonaws.com"
@@ -30,6 +35,10 @@ defmodule Formation.S3.Credential do
     |> validate_required([:type, :region, :access_key_id, :secret_access_key])
     |> validate_inclusion(:acl, ["private", "public"])
     |> maybe_validate_bucket()
+    |> maybe_set_access_key()
+    |> maybe_set_secret_key()
+    |> maybe_set_region()
+    |> maybe_set_endpoint()
   end
 
   def create(params) do
@@ -41,6 +50,38 @@ defmodule Formation.S3.Credential do
   defp maybe_validate_bucket(changeset) do
     if fetch_field!(changeset, :type) == :instance do
       validate_required(changeset, [:bucket])
+    else
+      changeset
+    end
+  end
+
+  defp maybe_set_access_key(changeset) do
+    if username = get_field(changeset, :username) do
+      put_change(changeset, :access_key_id, username)
+    else
+      changeset
+    end
+  end
+
+  defp maybe_set_secret_key(changeset) do
+    if password = get_field(changeset, :password) do
+      put_change(changeset, :secret_access_key, password)
+    else
+      changeset
+    end
+  end
+
+  defp maybe_set_region(changeset) do
+    if resource = get_field(changeset, :resource) do
+      put_change(changeset, :region, resource)
+    else
+      changeset
+    end
+  end
+
+  defp maybe_set_endpoint(changeset) do
+    if host = get_change(changeset, :host) do
+      put_change(changeset, :endpoint, host)
     else
       changeset
     end
