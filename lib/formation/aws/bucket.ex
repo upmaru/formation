44d1d9dcbc3f836309f17d9224f2d89ce585b5e0
Zack Siri <me@zacksiri.dev>
2023-12-13 18:19:51 +0700

    Add ability to detect changes and update bucket settings

--- lib/formation/aws/bucket.ex
+++ lib/formation/aws/bucket.ex
@@ -1,3 +1,14 @@
 defmodule Formation.Aws.Bucket do
   defstruct [:name, :acl, :cors]
+
+  def new(%{"bucket" => bucket, "acl" => acl}) do
+    %__MODULE__{
+      name: bucket,
+      acl: acl
+    }
+  end
+
+  defdelegate check_difference(operations, operation_name, existing, new),
+    to: __MODULE__.Difference,
+    as: :check
 end
